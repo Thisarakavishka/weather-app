85ed7f77d749b8d6fbd1d7b0cc9d09d64b10b266
thisarakavishka <thisarakavishka0@gmail.com>
2024-07-12 13:00:23 +0200

    add navigastions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,32 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React from 'react'
+import { Tabs } from 'expo-router'
+import TabBar from '../components/TabBar'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+const _layout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+    <Tabs
+      tabBar={props => <TabBar{...props} />}
+    >
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: "Home"
+        }}
+      />
+      <Tabs.Screen
+        name='location'
+        options={{
+          title: "Locations"
+        }}
+      />
+      <Tabs.Screen
+        name='setting'
+        options={{
+          title: "Setting"
+        }}
+      />
+    </Tabs>
+  )
 }
+
+export default _layout
